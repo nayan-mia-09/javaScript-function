a07fadb47d98b9a9d36e55a3da5e17317f4887b7
Nayan <dreamerboynayan@gmail.com>
2025-04-18 10:25:08 +0600

    Convert Fahtenheit to Celsius

--- functions.js
+++ functions.js
@@ -1,10 +1,19 @@
 
 
-function square(number){
-    return number * number;
-};
+// function square(number){
+//     return number * number;
+// };
 
-console.log(square(5));
-console.log(square(6));
-console.log(square(10));
-console.log(square(100));
+// console.log(square(5));
+// console.log(square(6));
+// console.log(square(10));
+// console.log(square(100));
+
+
+//Convert Fahrenheit to Celsius:
+function toCelcius(fahrenheit){
+    return ((5 / 9) * (fahrenheit - 32));
+};
+ 
+const resultDegree = toCelcius(95);
+console.log(resultDegree);
